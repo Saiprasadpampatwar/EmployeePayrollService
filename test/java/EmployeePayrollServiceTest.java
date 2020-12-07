7c9f4607dc87b6147e8398f895be784ee00efad4
SaiprasadPampatwar <pampatwarsaiprasad321@gmail.com>
2020-12-07 20:06:43 +0530

    [Saiprasad]Added: method write writeData to write data in payroll.txt file

--- test/java/EmployeePayrollServiceTest.java
+++ test/java/EmployeePayrollServiceTest.java
@@ -1,10 +1,33 @@
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+
+import static java.util.Arrays.*;
+
 public class EmployeePayrollServiceTest {
+
+    static EmployeePayrollService employeePayrollService;
+
+    @BeforeClass
+    public static void initializeConstructor()
+    {
+        employeePayrollService = new EmployeePayrollService();
+    }
 
     @Test
     public void CheckingClassCreatedAndPrintingWelComeMassage() {
         EmployeePayrollService employeePayrollService = new EmployeePayrollService();
         employeePayrollService.printWelcomeMessage();
+    }
+
+    @Test
+    public void given3EmployeesWhenWrittenToFileShouldMatchEmployeeEntries() {
+        EmployeePayrollData[] arrayOfEmps = {
+                new EmployeePayrollData(1, "Jeff Bezos", 100000.0),
+                new EmployeePayrollData(2, "Bill Gates", 200000.0),
+                new EmployeePayrollData(3, "Mark Zuckerberg", 300000.0)
+        };
+        employeePayrollService = new EmployeePayrollService(asList(arrayOfEmps));
+        employeePayrollService.writeEmployeePayrollData(EmployeePayrollService.IOService.FILE_IO);
     }
 }
