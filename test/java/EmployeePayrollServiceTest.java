7f9162777cf8ae90bf5536bb00baeb56f13203a9
SaiprasadPampatwar <pampatwarsaiprasad321@gmail.com>
2020-12-07 19:09:18 +0530

    [Saiprasad]Added: methods for reading and writing data to and from console in employee payroll service class

---
+++ test/java/EmployeePayrollServiceTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class EmployeePayrollServiceTest {
+
+    @Test
+    public void CheckingClassCreatedAndPrintingWelComeMassage() {
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        employeePayrollService.printWelcomeMessage();
+    }
+}
