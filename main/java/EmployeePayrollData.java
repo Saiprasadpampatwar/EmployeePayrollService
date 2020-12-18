9361af38666097b8799eaebb2bba2f317ffb0f6b
SaiprasadPampatwar <pampatwarsaiprasad321@gmail.com>
2020-12-18 22:37:34 +0530

    [Saiprasad]Added: test case to check the retrived data is valid or not also created readData method for fectching details from database

--- main/java/EmployeePayrollData.java
+++ main/java/EmployeePayrollData.java
@@ -1,13 +1,21 @@
+import java.time.LocalDate;
+
 public class EmployeePayrollData {
     public int id;
     public String name;
     public double salary;
+    private LocalDate start;
 
     public EmployeePayrollData(int id, String name, double salary) {
         this.id = id;
         this.name = name;
         this.salary = salary;
     }
+    public EmployeePayrollData(int id, String name, double salary, LocalDate start) {
+        this(id,name,salary);
+        this.start = start;
+    }
+
 
     @Override
     public String toString() {
---
+++ main/java/PayrollServiceException.java
@@ -0,0 +1,11 @@
+public class PayrollServiceException extends Exception {
+    enum ExceptionType{
+        CONNECTION_PROBLEM, RETRIEVAL_PROBLEM;
+    }
+
+    ExceptionType type;
+    PayrollServiceException(String message, ExceptionType type){
+        super(message);
+        this.type = type;
+    }
+}
